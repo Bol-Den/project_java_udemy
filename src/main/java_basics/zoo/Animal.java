3a61c22185488b7480c53822ae86ef6a79fddc27
Den <deniss.boltunovs@gmail.com>
2024-06-17 09:34:15 +0300

    dealership module

--- src/main/java_basics/zoo/Animal.java
+++ src/main/java_basics/zoo/Animal.java
@@ -1,11 +1,12 @@
 package java_basics.zoo;
 
+import java.util.Objects;
+
 abstract class Animal {
+    String name;
 
-     String name;
-     String color;
-     int age;
-
+    String color;
+    int age;
      Animal(String name, String color, int age){
          this.name = name;
          this.color = color;
@@ -26,6 +27,19 @@ abstract class Animal {
 
      public abstract void move();
 
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Animal animal = (Animal) o;
+        return age == animal.age && Objects.equals(name, animal.name) && Objects.equals(color, animal.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, color, age);
+    }
 
 
 }
--- src/main/java_basics/zoo/ZooApp.java
+++ src/main/java_basics/zoo/ZooApp.java
@@ -6,6 +6,8 @@ class ZooApp {
         Fish fish = new Fish("Mermaid", "gold", 10, 25);
         Bird bird = new Bird("Sparrow", "brown", 5);
         Sparrow sparrow = new Sparrow("First Sparrow", "gold", 51);
+        Sparrow sparrow1 = new Sparrow("First Sparrow", "gold", 51);
+
         AnimalMove animalMove = new AnimalMove();
 
 //        fish.move();
@@ -19,6 +21,10 @@ class ZooApp {
         animalMove.getAnimalMove(fish);
         animalMove.getAnimalMove(bird);
         animalMove.getAnimalMove(sparrow);
+
+        System.out.println("Is sparrows are equals: " + sparrow.equals(sparrow1));
+        System.out.println(sparrow.hashCode());
+        System.out.println(sparrow1.hashCode());
 
     }
 
