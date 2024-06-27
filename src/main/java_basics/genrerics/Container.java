7737396b80a5f91192bf69a0c2c63ece95d91ecc
Den <deniss.boltunovs@gmail.com>
2024-06-27 09:34:29 +0300

    generics

---
+++ src/main/java_basics/genrerics/Container.java
@@ -0,0 +1,26 @@
+package java_basics.genrerics;
+
+class Container<S,I> {
+
+    S item1;
+    I item2;
+
+    Container(S item1, I item2){
+        this.item1 = item1;
+        this.item2 = item2;
+    }
+
+    public S getItem1() {
+        return item1;
+    }
+
+    public I getItem2() {
+        return item2;
+    }
+
+    void printItems(){
+        System.out.println("Item 1: " + item1);
+        System.out.println("Item 2: " + item2);
+    }
+
+}
---
+++ src/main/java_basics/genrerics/ContainerApp.java
@@ -0,0 +1,48 @@
+package java_basics.genrerics;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+class ContainerApp {
+
+    public static void main(String[] args) {
+        Container<String, Integer> container = new Container<>("A", 10) ;
+        Container<String, String> container2 = new Container<>("A", "B") ;
+
+        container.printItems();
+        container2.printItems();
+
+        Set<String> mySet1 = new HashSet<>();
+        mySet1.add("one");
+        mySet1.add("apple");
+        mySet1.add("computer");
+
+        Set<String> mySet2 = new HashSet<>();
+        mySet2.add("one");
+        mySet2.add("two");
+        mySet2.add("monitor");
+
+        Set<String> resultSet = union(mySet1, mySet2);
+        System.out.println( resultSet);
+
+        Iterator<String> itr = resultSet.iterator();
+        while (itr.hasNext()){
+            // not working if use raw version of iterator i.e Iterator without type
+            String var = itr.next();
+//            System.out.println(itr.next());
+            System.out.println(var);
+
+        }
+
+    }
+
+    //// generic method
+    public static <E> Set<E> union(Set<E> set1, Set<E> set2){
+        Set<E> result = new HashSet<>(set1);
+        result.addAll(set2);
+        return result;
+    }
+
+}
