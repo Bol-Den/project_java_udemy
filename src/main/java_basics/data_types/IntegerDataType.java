a19d1368ef4e4a207ee93be6a5c8e0e35954ccb7
Den <deniss.boltunovs@gmail.com>
2024-07-08 10:33:00 +0300

    integer

---
+++ src/main/java_basics/data_types/IntegerDataType.java
@@ -0,0 +1,12 @@
+package java_basics.data_types;
+
+class IntegerDataType {
+
+    public static void main(String[] args) {
+
+        System.out.println("Integer minValue:" + Integer.MAX_VALUE);
+        System.out.println("Integer maxValue:" + Integer.MIN_VALUE);
+        System.out.println("Integer bytes:" + Integer.BYTES);
+
+    }
+}
